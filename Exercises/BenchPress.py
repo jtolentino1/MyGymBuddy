0d660a2aa5ef5c1a4029f511defcd7335988face
Kanchan <shrestha.kanch@gmail.com>
2022-02-20 04:00:07 -0700

    Organized Exercises

---
+++ Exercises/BenchPress.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+
+import cv2
+import mediapipe as mp
+import numpy as np
+import time
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+def calculate_angle(a,b,c):
+    a = np.array(a) # First
+    b = np.array(b) # Mid
+    c = np.array(c) # End
+    
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180.0/np.pi)
+    
+    if angle > 180.0:
+        angle = 360-angle
+        
+    return angle 
+
+def start(sets, reps):
+    cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+    sets_counter = 0 
+
+
+    while sets_counter < sets:
+        # Bench Press reps_counter variables
+        stage = None
+        reps_counter = 0
+
+        # Setup mediapipe instance
+        with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+            cap.isOpened()
+            while reps_counter < reps:
+                ret, frame = cap.read()
+                
+                # Recolor image to RGB
+                image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                image.flags.writeable = False
+            
+                # Make detection
+                results = pose.process(image)
+            
+                # Recolor back to BGR
+                image.flags.writeable = True
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                
+                # Extract landmarks
+                try:
+                    landmarks = results.pose_landmarks.landmark
+                    
+                    # Get coordinates
+                    shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+                                landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                    elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
+                             landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+                    wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+                             landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+                    
+                    # Setup status box
+                    cv2.rectangle(image, (0,0), (225,73), (245,117,16), -1)
+                    
+                    # Rep data
+                    cv2.putText(image, 'REPS', (15,12), 
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+                    cv2.putText(image, str(reps_counter), 
+                                (10,60), 
+                                cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+                    
+                    # Stage data
+                    cv2.putText(image, 'STAGE', (65,12), 
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+                    cv2.putText(image, stage, 
+                                (60,60), 
+                                cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+                    
+                    # Render detections
+                    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                            mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                            mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                                            )   
+
+                    # Calculate angle
+                    angle = calculate_angle(shoulder, elbow, wrist)
+                    
+                    # Visualize angle
+                    cv2.putText(image, str(angle), 
+                                tuple(np.multiply(elbow, [640, 480]).astype(int)), 
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                        )
+                    
+                    # Press reps_counter logic
+                    if angle < 60:
+                        stage = "up"
+                    elif angle > 160 and stage == "up":
+                        stage = "down"
+                        reps_counter +=1
+                except:
+                    pass
+                    
+    cap.release()
+    cv2.destroyAllWindows()              
+
+if __name__ == "__main__":
+    start()
