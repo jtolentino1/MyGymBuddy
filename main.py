e276f282de944f15a89f5f4126d04b6a524dcf44
Joshua Tolentino <75052173+jtolentino1@users.noreply.github.com>
2022-02-19 20:43:22 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import json
+import Curls
+import Squats
+# import Squats
+
+def main():
+    # routine = ["curls", "squats", "extensions"]
+    
+    # f = open("exercises.json")
+    # data = json.load(f)
+    # for i in routine:
+    #     joints = data["exercises"][i]["joints"]
+    #     angle = data["exercises"][i]["angles"]
+    #    # print(joints,angle)
+    #importCurls
+    Curls.start()
+
+
+if __name__ == "__main__":
+    main()
