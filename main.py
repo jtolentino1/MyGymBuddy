f097622f320758dd590cc94de49a358215ffbb93
Joshua Tolentino <joshuatolentino218@gmail.com>
2022-02-19 20:49:57 -0700

    init

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
 import json
 import Curls
 import Squats
-# import Squats
 
-def main():
-    # routine = ["curls", "squats", "extensions"]
-    
-    # f = open("exercises.json")
-    # data = json.load(f)
-    # for i in routine:
-    #     joints = data["exercises"][i]["joints"]
-    #     angle = data["exercises"][i]["angles"]
-    #    # print(joints,angle)
-    #importCurls
-    Curls.start()
-
-
-if __name__ == "__main__":
-    main()
+def main(arr):
+    for i in arr:
+        if i == 'Bicep Curl':
+            Curls.main()
+        elif i == 'Extensions':
+            Squats.main()
+        elif i == 'Squats':
+            Squats.main()
---
+++ starter.py
@@ -0,0 +1,30 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import streamlit as st 
+import main
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+st.title("MyGymBuddy")
+st.write("Select an exercise:")
+
+bicep_curl = st.checkbox('Bicep Curl')
+extensions = st.checkbox('Extensions')
+squats = st.checkbox('Squats')
+
+exercise_list = [bicep_curl, extensions, squats]
+exercises = ['Bicep Curl', 'Extensions', 'Squats']
+exercise_to_do = {}
+
+for count, item in enumerate(exercise_list):
+    if item:
+        user_input_rep = st.text_input("Please enter rep amount: " + exercises[count], key=count)
+        user_input_sets = st.text_input("Please enter set amount: " + exercises[count],key=count)
+        exercise_to_do[exercises[count]] = {"reps":user_input_rep,"sets":user_input_sets}
+
+options = st.button("Click me to begin.")
+if options:
+    st.write(exercise_to_do)
+    main.main(exercise_to_do)
